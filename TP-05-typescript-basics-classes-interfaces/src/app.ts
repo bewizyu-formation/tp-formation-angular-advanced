9771ab053b83be5e19fdc1849a734f995c2305b5
Hamza Hsain <hhsain@bewizyu.com>
2021-01-14 17:20:21 +0100

    TP-05

--- TP-05-typescript-basics-classes-interfaces/src/app.ts
+++ TP-05-typescript-basics-classes-interfaces/src/app.ts
@@ -1,19 +1,88 @@
 // TODO 1 : Creating interfaces
-
 // TODO 2 : Extending interfaces 
 
-// TODO 3 : Classes and constructors
- 
-// TODO 4 : Public and private members 
- 
-// TODO 5 : Setters and getters 
+interface User extends Token {
+    firstName: string;
+    lastName: string;
+    callBack(): void;
+}
+interface Token {
+    tokenData: string;
+}
 
+function createUser(): User {
+    return {
+        tokenData: 'My-data',
+        firstName: 'Hello',
+        lastName: 'Hello',
+        callBack() {
+        }
+    }
+}
+
+let myUser: User = createUser();
+// console.log(myUser);
+
+// TODO 3 : Classes and constructors
+// TODO 4 : Public and private and readonly members 
+// TODO 5 : Setters and getters
 // TODO 6 : Inheritance and abstract classes 
 
+abstract class GenericService {
+    constructor(public baseUrl: string){}
 
- 
+    public loadHost(){
+
+    }
+}
+
+class UserService extends GenericService {
+    
+    attribute : string 
+    constructor(
+        baseUrl : string,
+        readonly userNetworkDataSource: string,
+        private users: User[]
+    ) {
+        super(baseUrl);
+        this.attribute = '';
+    }
+
+    get usersList(){
+        console.log('get usersList')
+        return this.users;
+    }
+    
+    set usersList(val: User[]){
+        console.log('set usersList')
+        this.users = val;
+    }
 
 
- 
+    public fetchUsers(): User[] {
+        return [{
+            tokenData: 'My-data',
+            firstName: 'Hello',
+            lastName: 'Hello',
+            callBack() {
+            }
+        }];
+    }
+}
+
+const monService: UserService = new UserService('http://192.168.1.1', 'Ma variable', []);
+
+// const baseService : GenericService = new GenericService('http://192.168.1.1');
+
+monService.usersList = [];
+const result = monService.usersList;
+
+console.log(monService.baseUrl);
+
+
+
+
+
+
 
 
