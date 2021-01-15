2027a00cd328e0bd790a89e6f6533a9f4ea1e967
Hamza Hsain <hhsain@bewizyu.com>
2021-01-15 17:37:28 +0100

    TP Angular

--- TP-08-angular-templates-basics/src/app/quizz/components/quizz.component.ts
+++ TP-08-angular-templates-basics/src/app/quizz/components/quizz.component.ts
@@ -8,14 +8,63 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class QuizzComponent implements OnInit, OnDestroy {
 
-  quizzQuestions : Quizz[] = [];
+  currentQuestionIndex = 0;
+  quizzQuestions: Quizz[] = [
+    {
+      question: 'One of the deadliest pandemics, the Spanish Flu, killed off what percentage of the human world population at the time?',
+      answers: [{
+        answer: '3 to 6 percent',
+        isCorrect: true
+      },
+      {
+        answer: '6 to 10 percent',
+        isCorrect: false
+      },
+      {
+        answer: '1 to 3 percent',
+        isCorrect: false
+      },
+      {
+        answer: 'less than 1 percent',
+        isCorrect: false
+      }
+      ]
+    },
+    {
+      question: 'What is the nickname for the US state Delaware?',
+      answers: [{
+        answer: 'The Fiftieth State',
+        isCorrect: false
+      },
+      {
+        answer: 'The First State',
+        isCorrect: true
+      },
+      {
+        answer: 'The Second State',
+        isCorrect: false
+      },
+      {
+        answer: 'The Sixteenth State',
+        isCorrect: false
+      }
+      ]
+    }
+  ];
 
-  constructor( ) {
+  constructor() {
 
   }
 
   ngOnInit(): void {
 
+  }
+  nextQuestion(){
+    if(this.currentQuestionIndex < this.quizzQuestions.length -1 ){
+      this.currentQuestionIndex ++;
+    } else {
+      //
+    }
   }
 
   ngOnDestroy(): void {
