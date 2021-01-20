e43fd84d59a380384c114a00dd72defab4779354
Hamza Hsain <hhsain@bewizyu.com>
2021-01-20 16:22:55 +0100

    add search input

--- TP-14-rxjs-operators/index.js
+++ TP-14-rxjs-operators/index.js
@@ -1,5 +1,5 @@
-import { fromEvent, Observable, of } from 'rxjs';
-import { filter, map, mapTo, pluck } from 'rxjs/operators';
+import { fromEvent, interval, Observable, of, Subject } from 'rxjs';
+import { distinctUntilChanged, distinctUntilKeyChanged, filter, map, mapTo, pluck, reduce, scan, take, takeUntil, takeWhile, tap } from 'rxjs/operators';
 
 /*
  * Any code samples you want to play with can go in this file.
@@ -64,7 +64,76 @@ fromEvent(document, 'scroll')
         })
     )
     .subscribe(poucentage => {
-        // scrollTop / (scrollHeight - clientHeight)
-        // document.getElementById('')
-        document.getElementById('progress-bar').style.width = `${poucentage}%`
+        // document.getElementById('progress-bar').style.width = `${poucentage}%`
     })
+
+
+// OPERATEUR TAP 
+fromEvent(document, 'click')
+    .pipe(
+        tap(clickEvent => {
+            console.log('TAP IS HERE')
+        })
+    )
+
+// SCAN / Reduce OPERATOR : (avec memoire)
+
+interval(1000)
+    .pipe(
+        take(5),
+        scan((acc, val) => {
+            return acc + val;
+        })
+    )
+
+// Take, TakeWhile takeUntile 
+
+interval(1000)
+    .pipe(
+        scan((acc, val) => {
+            return acc + val;
+        }),
+        takeWhile(x => x < 10)
+    )
+
+
+const source2$ = new Subject();
+const source1$ = interval(1000);
+
+source1$
+    .pipe(
+        takeUntil(source2$)
+    )
+
+
+fromEvent(document, 'click')
+    .pipe(
+        tap(clickEvent => {
+            console.log('subject.next');
+            source2$.next(10);
+        })
+    )
+
+// distinctUntilChanged
+
+of({
+    name: 'Dupond',
+    age: 30
+}, {
+    name: 'Dupond',
+    age: 25
+}, {
+    name: 'Dupond2',
+    age: 40
+}).pipe(
+    distinctUntilKeyChanged('name')
+)
+
+// Ignorer les doublons et ajouter un debouce time de 300 - 500 ms pour optimiser les requetes de recherche
+fromEvent(document.getElementById('search-input'), 'keyup')
+    .pipe(
+        pluck('target', 'value'),
+        tap(ev => {
+            console.log(ev);
+        })
+    ).subscribe()
